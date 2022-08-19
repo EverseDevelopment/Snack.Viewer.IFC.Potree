6d92b42cfbece9f349515892283f26d15d52b9de
Pablo Derendinger <pderendinger@e-verse.com>
2022-08-19 12:05:32 -0300

    adding the IFC files for demo

---
+++ pointclouds/demo/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 8772682,
+    "boundingBox": {
+        "lx": -10.221177101135254,
+        "ly": -5.240261077880859,
+        "lz": -0.04206699877977371,
+        "ux": 3.508918046951294,
+        "uy": 8.489834070205689,
+        "uz": 13.688028149306775
+    },
+    "tightBoundingBox": {
+        "lx": -10.2211771,
+        "ly": -5.2402611,
+        "lz": -0.042067,
+        "ux": 3.508918,
+        "uy": 2.9466528999999999,
+        "uz": 5.4539642
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.11890611052513123,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
